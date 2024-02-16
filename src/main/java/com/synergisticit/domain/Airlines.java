a320b39ad0c7ae59a0588374170f29602e868005
Jason Lee <jslee423@gmail.com>
2024-02-15 19:16:30 -0500

    add jsonBackReference for airlines flights

--- src/main/java/com/synergisticit/domain/Airlines.java
+++ src/main/java/com/synergisticit/domain/Airlines.java
@@ -3,6 +3,8 @@ package com.synergisticit.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -28,6 +30,7 @@ public class Airlines {
 	
 	private String airlinesCode;
 	
+	@JsonBackReference
 	@OneToMany(mappedBy = "airlines")
 	private List<Flight> flights = new ArrayList<>();
 }
